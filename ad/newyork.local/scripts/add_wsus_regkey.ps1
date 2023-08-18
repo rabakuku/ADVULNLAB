b12c81cbe20efe2e3732a65319e8dbfdcff47133
rabakuku <71357873+rabakuku@users.noreply.github.com>
2023-08-17 21:51:16 -0400

    Delete add_wsus_regkey.ps1

--- ad/newyork.local/scripts/add_wsus_regkey.ps1
+++
@@ -1,9 +0,0 @@
-Set-ItemProperty -Path "HKLM:\software\policies\Microsoft\Windows\WindowsUpdate" -Name WUServer -Value "http://192.168.56.24:8530" -Type String -force
-Set-ItemProperty -Path "HKLM:\software\policies\Microsoft\Windows\WindowsUpdate" -Name WUStatusServer -Value "http://192.168.56.24:8530" -Type String -force
-Set-ItemProperty -Path "HKLM:\software\policies\Microsoft\Windows\WindowsUpdate\AU" -Name UseWUServer -Value "1" -Type DWORD -force
-net start BITS
-net start CryptSvc
-$updateSession = new-object -com "Microsoft.Update.Session"; $updates=$updateSession.CreateupdateSearcher().Search($criteria).Updates
-USOClient scannow
-wuauclt.exe /detectnow /reportnow
-
